c23c6783c8e6e59095b2069a961d42989835429c
Igor Shadurin <igor.shadurin@gmail.com>
2024-05-30 17:38:30 +0100

    refactor: auth

--- api/index.tsx
+++ api/index.tsx
@@ -1,15 +1,17 @@
 import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
+import dappykit from '@dappykit/sdk'
 import { serveStatic } from 'frog/serve-static'
 import { configureApp } from './utils/frame.js'
-import { BORDER_FAIL, BORDER_SIMPLE, BORDER_SUCCESS, Box, Heading, Text, VStack } from './utils/style.js'
+import { BORDER_FAIL, BORDER_SIMPLE, BORDER_SUCCESS, Box, Heading, Text, vars, VStack } from './utils/style.js'
 import { handle } from 'frog/vercel'
 import quizData from '../quiz.json' assert { type: 'json' }
 import { Quiz } from './quiz/index.js'
+import { kvGetDelegatedAddress, kvPutMnemonic } from './utils/kv.js'
 
-// const { ViemUtils, Utils } = dappykit
-// const { generateMnemonic, privateKeyToAccount, english, mnemonicToAccount } = ViemUtils
-// const { accountToSigner } = Utils.Signer
+const { ViemUtils, Utils } = dappykit
+const { generateMnemonic, privateKeyToAccount, english, mnemonicToAccount } = ViemUtils
+const { accountToSigner } = Utils.Signer
 
 // todo implement Quiz using the class
 // todo add info about hackathon
@@ -18,6 +20,7 @@ import { Quiz } from './quiz/index.js'
 export const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
+  ui: { vars },
 })
 
 app.frame('/', async c => {
@@ -78,6 +81,9 @@ app.frame('/next', async c => {
           <Heading color="h1Text" align="center" size="64">
             {quiz.questions[questionIndex].question}
           </Heading>
+          <Text align="center" size="18">
+            Question: {questionIndex + 1}/{quiz.questions.length}
+          </Text>
         </VStack>
       </Box>
     ),
@@ -86,15 +92,22 @@ app.frame('/next', async c => {
 })
 
 app.frame('/result', async c => {
-  const { appTitle } = await configureApp(app, c)
+  const { appTitle, userMainAddress } = await configureApp(app, c)
   const buttonData = JSON.parse(c.buttonValue || '{}')
   const quiz = new Quiz(quizData)
   const points = buttonData.p ? Number(buttonData.p) : 0
   const pointsText = `${points.toString()} of ${quiz.questions.length}`
   const isWin = points === quiz.questions.length
   const resultText = isWin ? "That's right! Well done!" : 'You can do better!'
+  const userDelegatedAddress = await kvGetDelegatedAddress(userMainAddress)
+  const intents = [<Button action="/">🔁 Again</Button>]
 
-  const intents = [<Button action="/">OK</Button>]
+  if (!isWin) {
+    // if user authorized direct to answers, if not direct to authorize
+    intents.push(<Button action={userDelegatedAddress ? '/answers' : '/authorize'}>🙋 Answers</Button>)
+  }
+
+  intents.push(<Button.Link href="https://hack.dappykit.org/?source=quiz-template">🔴 Earn 35-5k OP</Button.Link>)
 
   return c.res({
     title: appTitle,
@@ -119,6 +132,138 @@ app.frame('/result', async c => {
     intents,
   })
 })
+
+/**
+ * Checks that user already authorized the app and shows him the button to show the answers.
+ *
+ * Cases when executed:
+ * 1. User clicks to show answers the first time. Should create Auth Request.
+ * 2. User already sends Auth Request and is waiting for the answer.
+ */
+app.frame('/authorize', async c => {
+  const { appTitle, userMainAddress, appAuthUrl, appPk, dappyKit, messageBytes } = await configureApp(app, c)
+  const userDelegatedAddress = await kvGetDelegatedAddress(userMainAddress)
+  const isCheckStatus = c.buttonValue === 'check-status'
+  let intents = []
+  let text = ''
+  let errorText = ''
+  let response
+
+  if (userDelegatedAddress) {
+    text = '✅ The application is authorized! You can view the answers.'
+    intents = [<Button action={'/answers'}>🙋 Answers</Button>]
+  } else {
+    if (isCheckStatus) {
+      text = `⏳ Waiting...`
+      intents = [
+        <Button value="check-status" action="/authorize">
+          Check Status
+        </Button>,
+        <Button.Reset>🏠 Home</Button.Reset>,
+      ]
+    } else {
+      try {
+        const appSigner = accountToSigner(privateKeyToAccount(appPk))
+        const userDelegatedMnemonic = generateMnemonic(english)
+        const userDelegatedWallet = mnemonicToAccount(userDelegatedMnemonic)
+        response = await dappyKit.farcasterClient.createAuthRequest(
+          messageBytes,
+          userDelegatedWallet.address,
+          appSigner,
+        )
+
+        if (response.status !== 'ok') {
+          throw new Error(`Invalid auth response status. ${JSON.stringify(response)}`)
+        }
+
+        await kvPutMnemonic(userDelegatedWallet.address, userDelegatedMnemonic)
+      } catch (e) {
+        const error = (e as Error).message
+        console.log('Auth request error', error) // eslint-disable-line no-console
+        errorText = `Error: ${error}`
+      }
+
+      text = `❓To view the answers, click "Authorize" and enter the number ${response?.answer}.`
+      intents = [
+        <Button.Link href={appAuthUrl}>🐙 Authorize</Button.Link>,
+        <Button value="check-status" action="/authorize">
+          Check Status
+        </Button>,
+      ]
+    }
+  }
+
+  return c.res({
+    title: appTitle,
+    image: (
+      <Box grow alignVertical="center" backgroundColor="white" padding="32" border={BORDER_SIMPLE}>
+        <VStack gap="4">
+          <Heading color="h1Text" align="center" size="48">
+            {errorText && 'Error'}
+            {!errorText && text}
+          </Heading>
+
+          <Text align="center" size="18">
+            {errorText && `Error: ${errorText}`}
+          </Text>
+        </VStack>
+      </Box>
+    ),
+    intents,
+  })
+})
+
+// app.frame('/answers', async c => {
+//   const { appTitle, userMainAddress, dappyKit, appAddress } = await configureApp(app, c, 'appAuthUrl')
+//   const userDelegatedAddress = await kvGetDelegatedAddress(userMainAddress)
+//   let intents = []
+//   let text = ''
+//
+//   if (userDelegatedAddress) {
+//     text = 'The application is authorized! You can manage user information using the buttons below.'
+//     intents = [
+//       <Button value="info" action="/info">
+//         Info
+//       </Button>,
+//       <Button value="save" action="/save">
+//         Save Data
+//       </Button>,
+//       <Button value="reset-delegated" action="/reset-delegated">
+//         Reset Delegated Address
+//       </Button>,
+//     ]
+//   } else {
+//     text = 'This is an example of DappyKit integration. Click "Auth Request" to authorize the app.'
+//     intents = [
+//       <Button value="auth-request" action="/auth-request">
+//         Auth Request
+//       </Button>,
+//       <Button.Reset>Back</Button.Reset>,
+//     ]
+//   }
+//
+//   // todo check is user authorized, if yes, show question-answer one by one
+//
+//   // const intents = [<Button action="/next">Start</Button>]
+//
+//   return c.res({
+//     title: appTitle,
+//     image: (
+//       <Box grow alignVertical="center" backgroundColor="white" padding="32" border={BORDER_SIMPLE}>
+//         <VStack gap="4">
+//           <Heading color="h1Text" align="center" size="64">
+//             Quiz time!
+//           </Heading>
+//
+//           <Text align="center" size="18">
+//             {quizData.shortDescription}
+//           </Text>
+//         </VStack>
+//       </Box>
+//     ),
+//     intents,
+//   })
+// })
 
 // app.frame('/', async c => {
 //   const { appTitle } = await configureApp(app, c)
